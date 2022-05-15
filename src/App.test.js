f9f8e46a62ff99530cad3b2c6eba7343f775f990
Nicolás Londoño <nicolas.londono@veevart.com>
2022-05-14 23:30:41 -0500

    refactor: remove unused files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders pokemon api', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/pokemon api/i);
   expect(linkElement).toBeInTheDocument();
 });
