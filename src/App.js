b8ff3a97e61ac6a0bd84f6162b9dccde07795e7b
Nicolás Londoño <nicolas.londono@veevart.com>
2022-05-15 22:10:55 -0500

    fix: format files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import Header from './components/Header/Header';
 
 function App() {
-  return (
-    <div className="app">
-      <Header />
-      <main>
-        <p>Contenido</p>
-      </main>
-    </div>
-  );
+	return (
+		<div className="app">
+			<Header />
+			<main>
+				<p>Contenido</p>
+			</main>
+		</div>
+	);
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,9 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('should render app', async () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Contenido/i);
-    expect(linkElement).toBeInTheDocument();
-  });
+	it('should render app', async () => {
+		render(<App />);
+		const linkElement = screen.getByText(/Contenido/i);
+		expect(linkElement).toBeInTheDocument();
+	});
 });
