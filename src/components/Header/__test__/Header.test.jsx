cfdc0d362c13e5d1b3e5f5a588e9ed8deea00af1
Nicolás Londoño <nicolas.londono@veevart.com>
2022-05-15 22:16:25 -0500

    fix: run prettier

--- src/components/Header/__test__/Header.test.jsx
+++ src/components/Header/__test__/Header.test.jsx
@@ -2,9 +2,9 @@ import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
 describe('Header', () => {
-  it('should render component', async () => {
-    render(<Header />);
-    const headerElement = screen.getByText(/Pokémon api/i);
-    expect(headerElement).toBeInTheDocument();
-  });
+	it('should render component', async () => {
+		render(<Header />);
+		const headerElement = screen.getByText(/Pokémon api/i);
+		expect(headerElement).toBeInTheDocument();
+	});
 });
